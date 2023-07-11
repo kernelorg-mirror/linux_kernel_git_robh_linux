15ce79ff865fa5bee4d0e56fcf30b85e6e08334b
Rob Herring <robh@kernel.org>
2023-07-11 10:06:14 -0600

    clk: Explicitly include correct DT includes
    The DT of_device.h and of_platform.h date back to the separate
    of_platform_bus_type before it as merged into the regular platform bus.
    As part of that merge prepping Arm DT support 13 years ago, they
    "temporarily" include each other. They also include platform_device.h
    and of.h. As a result, there's a pretty much random mix of those include
    files used throughout the tree. In order to detangle these headers and
    replace the implicit includes with struct declarations, users need to
    explicitly include the correct includes.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/clk/axs10x/pll_clock.c
+++ drivers/clk/axs10x/pll_clock.c
@@ -12,8 +12,8 @@
 #include <linux/err.h>
 #include <linux/device.h>
 #include <linux/io.h>
+#include <linux/of.h>
 #include <linux/of_address.h>
-#include <linux/of_device.h>
 #include <linux/slab.h>
 #include <linux/of.h>
 
--- drivers/clk/bcm/clk-bcm63xx-gate.c
+++ drivers/clk/bcm/clk-bcm63xx-gate.c
@@ -3,7 +3,7 @@
 #include <linux/clk-provider.h>
 #include <linux/init.h>
 #include <linux/of.h>
-#include <linux/of_device.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 
 #include <dt-bindings/clock/bcm3368-clock.h>
