4d6fd05d2ef813af8dfd7f9c5f024d256d7f9196
Rob Herring (Arm) <robh@kernel.org>
2025-06-23 16:47:33 -0500

    ethos-test: Fixes and add BO wait
    Signed-off-by: Rob Herring (Arm) <robh@kernel.org>

--- ethos_test.c
+++ ethos_test.c
@@ -39,13 +39,23 @@ void *bo_create(int fd, int size, bo_handle *handle)
 	ioctl(fd, DRM_IOCTL_ETHOS_BO_MMAP_OFFSET, &mmap_offset);
 
 	buf = mmap(NULL, bo_create.size, PROT_WRITE | PROT_READ,
-		   MAP_PRIVATE, fd, mmap_offset.offset);
+		   MAP_SHARED, fd, mmap_offset.offset);
 	if (!buf)
 		return NULL;
 
 	*handle = bo_create.handle;
 	return buf;
 }
+
+int bo_wait(int fd, bo_handle handle)
+{
+	struct drm_ethos_bo_wait bo_wait = {
+		.handle = handle,
+		.timeout_ns = INT64_MAX,
+	};
+	return ioctl(fd, DRM_IOCTL_ETHOS_BO_WAIT, &bo_wait);
+}
+
 
 bo_handle cmd_bo_create(int fd, void *buf, int size)
 {
@@ -77,13 +87,15 @@ int submit_job(int fd, bo_handle cmd, bo_handle *region_bos)
 
 }
 
+#define BO_SIZE 0x00100000UL
+
 uint32_t cmds[] = {
 	0x00000123,
-	0x00000130,
+	0x00000130, // cmd0.NPU_SET_DMA0_SRC_REGION
 	0x00004030, 0x00000000, // cmd1.NPU_SET_DMA0_SRC
 	0x00020131, // cmd0.NPU_SET_DMA0_DST_REGION
 	0x00004031, 0x00000000, // cmd1.NPU_SET_DMA0_DST
-	0x00004032, 0x00001000, // cmd1.NPU_SET_DMA0_LEN
+	0x00004032, BO_SIZE, // cmd1.NPU_SET_DMA0_LEN
 	0x00000010, // cmd0.NPU_OP_DMA_START
 	0x00000011, // cmd0.NPU_OP_DMA_WAIT               0
 	0xffff0000, // cmd0.NPU_OP_STOP               65535
@@ -91,25 +103,29 @@ uint32_t cmds[] = {
 
 int main(int argc, char **argv)
 {
-	int fd;
+	int fd, ret;
 	bo_handle cmd_handle;
 	bo_handle region_bos[8] = {};
 	uint32_t *src_bo, *dst_bo;
 
-	fd = open("/dev/accel/accel0", 0, O_RDWR);
+	fd = open("/dev/accel/accel0", O_RDWR | O_CLOEXEC);
 	dev_query(fd);
 
-	src_bo = bo_create(fd, 0x1000, &region_bos[0]);
-	dst_bo = bo_create(fd, 0x1000, &region_bos[2]);
+	src_bo = bo_create(fd, BO_SIZE, &region_bos[0]);
+	dst_bo = bo_create(fd, BO_SIZE, &region_bos[2]);
 
-	for (int i = 0; i < 0x1000/4; i++)
+	for (int i = 0; i < BO_SIZE/4; i++)
 		src_bo[i] = 0xdeadbeef;
 
 	printf("cmd buffer = %p\n", cmds);
 	cmd_handle = cmd_bo_create(fd, cmds, sizeof(cmds));
 	submit_job(fd, cmd_handle, region_bos);
 
-	sleep(1);
-	printf("0x%x 0x%x 0x%x 0x%x\n", dst_bo[0], dst_bo[1], dst_bo[2], dst_bo[3]);
+	ret = bo_wait(fd, region_bos[2]);
+	if (ret)
+		printf("error waiting on BO - %d\n", ret);
+
+	printf("src %llx: 0x%x 0x%x 0x%x 0x%x\n", src_bo, src_bo[0], src_bo[1], src_bo[2], src_bo[3]);
+	printf("dst %llx: 0x%x 0x%x 0x%x 0x%x\n", dst_bo, dst_bo[0], dst_bo[1], dst_bo[2], dst_bo[3]);
 	return 0;
 }
